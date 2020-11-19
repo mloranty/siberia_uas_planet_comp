14021593dfb534f79be8dea3c77e2478376ec36d
Mike Loranty <mloranty@colgate.edu>
2020-11-19 09:05:00 -0500

    updated data pre-processing and RF classification scripts

--- geospatial_data_prep.R
+++ geospatial_data_prep.R
@@ -30,48 +30,93 @@ aea <- "+proj=aea +lat_1=25 +lat_2=75 +lat_0=60 +lon_0=150 +x_0=0 +y_0=0 +ellps=
 #------------load rgb uas data------------#
 # Cherskiy North Transect 1
 tr1 <- stack("data/uas/CYN_TR1_FL016.tif")
-# set coordinate system
 
+# set the NA value 
+NAvalue(tr1) <- 255
+
+# set coordinate system
 crs(tr1) <- st 
-# Cherskiy North Transect 2
-tr2 <- stack("data/uas/CYN_TR2_FL017.tif")
-# set coordinate system
-crs(tr2) <- st 
 
-# create from merged extent object to clip Planet image
-x1 <- projectExtent(tr1,crs(pl))
-x2 <- projectExtent(tr2,crs(pl))
+# # Cherskiy North Transect 2
+# tr2 <- stack("data/uas/CYN_TR2_FL017.tif")
+# # set coordinate system
+# crs(tr2) <- st 
 
-x <- merge(extent(projectExtent(tr1,crs(pl))),
-           extent(projectExtent(tr2,crs(pl))))
+tr1a <- projectRaster(tr1,crs = aea, res = 0.02,method = "bilinear",
+                      filename = "data/uas/CYN_TR1_FL016_aea.tif")
+
+# create bounding mask
+m <- reclassify(tr1[[1]],c(0,254,1),
+                filename = "data/uas/CYN_TR1_mask.tif")
+
+# reproject drone imagery (this takes a long time!)
+tr1a <- projectRaster(tr1,crs = aea, res = res(tr1),
+                      ) 
+#tr1l <- projectRaster(tr1,crs = laea, res = res(tr1))
+
+#------------load multispectral uas data------------#
+##
+# read multispectral data
+tr1m <- stack(c("data/uas/updated/RU_CYN_TR1_FL016M_index_green.tif",
+                "data/uas/updated/RU_CYN_TR1_FL016M_index_red.tif",
+                "data/uas/updated/RU_CYN_TR1_FL016M_index_red_edge.tif",
+                "data/uas/updated/RU_CYN_TR1_FL016M_index_nir.tif",
+                "data/uas/updated/RU_CYN_TR1_FL016M_index_ndvi.tif"))
+
+
+# calculate ndwi after McFeeters et al 
+tr1m[[6]] <- (tr1m[[1]]-tr1m[[4]])/(tr1m[[1]]+tr1m[[4]])
+
+# # set band names for ms image
+names(tr1m) <- c("green", "red", "rededge", "nir", "ndvi", "ndwi")
+
+# reproject to mask MS data
+m2 <- projectRaster(m,tr1m[[1]], method = "ngb",
+                    filname = "data/processed/CYN_TR1_UTM_mask.tif" )
+
+tr1m <- mask(tr1m,m2, 
+             filename = "data/processed/RU_CYN_TR1_FL016M_allbands_rgb_mask.tif",
+             overwrite = T)
+
 
 #------------load planet satellite data------------#
+##
 # planet analytic image from 6 July 2019
 pl <- stack("data/CYN_06_Jul_2019_PSScene4Band_Explorer/files/PSScene4Band/20190706_002918_101b/analytic_sr_udm2/20190706_002918_101b_3B_AnalyticMS_SR.tif")
 
-# crop planet to extent of drone flights
-test <- crop(pl,x)
+# create from extent object to clip Planet image
+# x <- merge(extent(projectExtent(tr1,crs(pl))),
+#            extent(projectExtent(tr2,crs(pl))))
 
-# reproject Planet image to ...
-plr <- projectRaster(pl,crs = st, res = 3,
-                     filename = "CYN_stereo_20190706_002918_101b_3B_AnalyticMS_SR.tif")
+# crop planet to extent of RGBdrone flights
+tr1pl <- crop(pl,projectExtent(tr1,crs(pl)))
 
+# resample to mask planet data
+m3 <- resample(m2,tr1pl[[1]], method = "ngb",
+                    filname = "data/processed/CYN_TR1_UTM_mask.tif" )
 
-plr <- crop(plr,merge(extent(tr1),extent(tr2)),
-            filename = "CYN_stereo_20190706_002918_101b_3B_AnalyticMS_SR.tif")
+# mask planet data to match extent of UAS data
+tr1pl <- mask(tr1pl, m3,
+              filename = "data/processed/CYN_TR1_rgb_mask_20190706_002918_101b_3B_AnalyticMS_SR.tif")
 
-# reproject drone imagery
-tr1a <- projectRaster(tr1,crs = aea, res = res(tr1),
-                      ) 
-tr1l <- projectRaster(tr1,crs = laea, res = res(tr1))
+#calculate ndvi
+tr1pl$ndvi <- (tr1pl$nir-tr1pl$red)/(tr1pl$nir+tr1pl$red)
+
+#calculate ndwi
+tr1pl$ndwi <- (tr1pl$green-tr1pl$nir)/(tr1pl$green+tr1pl$nir)
+
+#write output to file
+writeRaster(tr1pl, filename = "data/processed/CYN_TR1_rgb_mask_20190706_002918_101b_3B_AnalyticMS_SR.tif",
+            overwrite = T)
 
 #------------load training/validation data------------#
 # read in  data
 lc <- readOGR("data/ground_truth/cyn_landover_pts.shp")
 
-#------------load multispectral uas data------------#
 
-# Kolym_Albers_Equal_Area_Conic_2
+
+
+# Kolyma_Albers_Equal_Area_Conic_2
 # Authority: Custom
 # 
 # Projection: Albers
--- rf_classification.R
+++ rf_classification.R
@@ -20,10 +20,12 @@ library(randomForest)
 setwd("L:/projects/siberia_uas_planet_comp")
 
 #------------load data sets------------#
-
-# load rbg flight from Cherskiy North Transect 1
+## 
+#rbg flight from Cherskiy North Transect 1
 tr1 <- stack("data/uas/CYN_TR1_FL016.tif")
 
+# set the NA value 
+NAvalue(tr1) <- 255
 # set band names for rgb image
 names(tr1) <- c("B1", "B2", "B3")
 
@@ -32,6 +34,19 @@ st <- "+proj=stere +lat_0=90 +lat_ts=71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84
 
 # set coordinate system
 crs(tr1) <- st 
+
+##
+# read parrot ms data
+tr1m <- stack("data/processed/RU_CYN_TR1_FL016M_allbands_rgb_mask.tif")
+
+# set band names for ms image
+names(tr1m) <- c("green", "red", "rededge", "nir", "ndvi", "ndwi")
+
+##
+# read planet data
+pl <- stack("data/processed/CYN_TR1_rgb_mask_20190706_002918_101b_3B_AnalyticMS_SR.tif")
+
+names(pl) <- c("blue", "green", "red", "nir", "ndvi", "ndwi")
 
 # read in training/validation data
 lc <- readOGR("data/ground_truth/cyn_landover_pts.shp")
@@ -61,14 +76,23 @@ landExtract$sampleType[sampleSamp] <- "valid"
 
 # extract raster values for training data points
 rgbEx <- extract(tr1,lc)
+msEx <- extract(tr1m,lc)
+plEx <- extract(pl,lc)
 
 # join the data
 dataAll <- cbind(landExtract,rgbEx)
+dataAllms <- cbind(landExtract,msEx)
+dataAllpl <- cbind(landExtract,plEx)
 
 # create training and validation subsets
 trainD <- dataAll[dataAll$sampleType=="train",]
 validD<- dataAll[dataAll$sampleType=="valid",]
 
+trainDms <- dataAllms[dataAllms$sampleType=="train",]
+validDms <- dataAllms[dataAllms$sampleType=="valid",]
+
+trainDpl <- dataAllpl[dataAllpl$sampleType=="train",]
+validDpl <- dataAllpl[dataAllpl$sampleType=="valid",]
 #------------Random Forest Classification of RGB data------------#
 # run the Random Forest model
 tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
@@ -89,11 +113,58 @@ rf_model
 
 # apply RF model to rgb data
 rf_prediction <- raster::predict(tr1, model=rf_model,
-                                 filename = "CYN_tr1_rgb_rf_lc.tif",
+                                 filename = "data/processed/CYN_tr1_rgb_rf_lc.tif",
                                  progress = T)
 
 #plot the data
 plot(rf_prediction)
 
 #------------Random Forest Classification of MS data------------#
+tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
+                   number = 10, # number 10 fold
+                   repeats = 10) # number of repeats
+###random forests
+#Typically square root of number of variables
+rf.grid <- expand.grid(mtry=1:3) # number of variables available for splitting at each tree node
 
+rf_model_ms <- caret::train(x = trainDms[,c(5:10)], #digital number data
+                         y = as.factor(trainDms$lcID), #land class we want to predict
+                         method = "rf", #use random forest
+                         metric="Accuracy", #assess by accuracy
+                         trainControl = tc, #use parameter tuning method
+                         tuneGrid = rf.grid) #parameter tuning grid
+#check output
+rf_model_ms
+
+# apply RF model to rgb data
+rf_prediction_ms <- raster::predict(tr1m, model=rf_model_ms,
+                                 filename = "data/processed/CYN_tr1_ms_rf_lc.tif",
+                                 progress = T)
+
+#plot the data
+plot(rf_prediction_ms)
+
+#------------Random Forest Classification of Planet data------------#
+tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
+                   number = 10, # number 10 fold
+                   repeats = 10) # number of repeats
+###random forests
+#Typically square root of number of variables
+rf.grid <- expand.grid(mtry=1:3) # number of variables available for splitting at each tree node
+
+rf_model_pl <- caret::train(x = trainDpl[,c(5:10)], #digital number data
+                            y = as.factor(trainDpl$lcID), #land class we want to predict
+                            method = "rf", #use random forest
+                            metric="Accuracy", #assess by accuracy
+                            trainControl = tc, #use parameter tuning method
+                            tuneGrid = rf.grid) #parameter tuning grid
+#check output
+rf_model_pl
+
+# apply RF model to Planet data
+rf_prediction_pl <- raster::predict(pl, model=rf_model_pl,
+                                    filename = "data/processed/CYN_tr1_planet_rf_lc.tif",
+                                    progress = T)
+
+#plot the data
+plot(rf_prediction_pl)
